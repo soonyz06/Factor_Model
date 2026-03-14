5b66a0d8d79c9da825ada3a791582ac0f685a96e
soonyz06 <soon.yz061025@gmail.com>
2026-03-14 11:12:46 +1100

    Add files via upload

--- Barra/main.py
+++ Barra/main.py
@@ -4,19 +4,20 @@ from datetime import datetime
 from pathlib import Path
 import re
 from functools import reduce
+import time
 
 
-class History:
+class Loader:
     def __init__(self):
-        self.basepath = Path.cwd() / "data" / "history"
+        self.basepath = Path.cwd() / "data" 
         self.basepath.mkdir(exist_ok=True, parents=True)
-        self.filepath = self.basepath / "history.parquet"
-
         self.identifiers = ["symbol", "date"]
-        
-    def fetch_history(self, symbols, period, interval="1d"):
+         
+    def fetch_history(self, symbols, period, interval="1d"): 
+        assert isinstance(symbols, list), "Symbols should be a list"
         if len(symbols)==0: return None
-        print(f"Fetching History ({period},{interval})-> {len(symbols)}")
+        
+        print(f"Fetching History ({period},{interval}) -> {len(symbols)}")
         ticker = Ticker(symbols, asynchronous=True)
         history = ticker.history(period=period, interval=interval)
         if history.empty:
@@ -27,107 +28,200 @@ class History:
         history = history.with_columns(pl.col("date").cast(pl.Date))
         return history
 
-    def load_history(self, symbols):
-        try:
-            history = pl.read_parquet(self.filepath)
-        except:
-            history = self.fetch_history(symbols, period="max")
-            history.write_parquet(self.filepath)
-            return history
-
-        missing = set(symbols) - set(history["symbol"].unique()) #unique is faster
-        missing_df = self.fetch_history(missing, period="max")
-
-        #date check + missing
+    def fetch_profile(self, symbols):
+        assert isinstance(symbols, list), "Symbols should be a list"
+        if len(symbols)==0: return None
+        print(f"Fetching Profile -> {len(symbols)}")
+        ticker = Ticker(symbols, asynchronous=True)
+        profile = pl.DataFrame({"symbol": key, **val} for key, val in ticker.summary_profile.items()).select(["symbol", "country", "industry", "sector"])
+        return profile
         
-        if missing_df is None or missing_df.is_empty():
-            return history
-        
-        history = pl.concat([history, missing_df])
-        history.write_parquet(self.filepath)
-        return history
+    def load_data(self, symbols, dirname, fetch_func, other_args=None): #add batching + updating + compact 
+        if other_args is None: other_args = {}
+        dirpath = self.basepath / dirname
+        dirpath.mkdir(exist_ok=True, parents=True)
+        existing_symbols = set()
+
+        if list(dirpath.glob("*.parquet")):
+            try:
+                lf = (pl.scan_parquet(dirpath / "*.parquet").select("symbol").unique().collect())
+                existing_symbols = set(lf["symbol"])
+            except (FileNotFoundError, pl.exceptions.ComputeError):
+                pass
+            except Exception as e:
+                print(f"[ERROR] Failed to load files due to: {e}")
+
+        missing_symbols = list(set(symbols) - existing_symbols)
+        if missing_symbols:
+            #print(f"Fetching missing symbols -> {len(missing_symbols)}")
+            missing_df = fetch_func(missing_symbols, **other_args)
+
+            if missing_df is not None and not missing_df.is_empty():
+                ts = datetime.now().strftime("%Y%m%d_%H%M%S")
+                missing_df = missing_df.with_columns(
+                    pl.lit(ts).alias("ts")
+                )
+                missing_df.write_parquet(dirpath / f"{ts}.parquet")
+
+        if not list(dirpath.glob("*.parquet")):
+            print("[ERROR] Failed load.")
+            return None
+    
+        lf =  pl.scan_parquet(dirpath / "*.parquet")
+        return lf
+
+class Processor:
+    def __init__(self):
+        self.basepath = Path.cwd() / "data" 
+        self.basepath.mkdir(exist_ok=True, parents=True)
+        self.identifiers = ["symbol", "date"]
 
     def log_transform(self, df):
+        #Chose to use log ret instead of raw ret due to the following:
+        #ln(a/b) = ln(a)-ln(b) ~ additive 
+        #ln(20/10) + ln(5/10) = 0 ~ symmetry
+        
         return (
-            df.sort(self.identifiers)
-            .upsample(time_column="date", every="1d", group_by="symbol")
+            df.sort(self.identifiers).upsample(time_column="date", every="1d", group_by="symbol") #ensures trading day alignment
             .with_columns(
-                pl.col("close").forward_fill().over("symbol") 
+                pl.col("adjclose").forward_fill().over("symbol") 
             )
-            .lazy()
-            .filter(pl.col("date").dt.weekday()<=5) #trading days only
+            .lazy() 
+            .filter(pl.col("date").dt.weekday()<=5) 
             .with_columns(
-                log_ret = pl.col("close").log().diff().over("symbol").fill_null(0)
+                log_ret = pl.col("adjclose").log().diff().over("symbol").fill_null(0)
             )
         )
 
-    def add_log_change(self, factor, df, lookback_days, gap_days):
-        #ln(a/b) = ln(a)-ln(b) (additive)
-        #ln: 10->20 == 10-> 5 (symmetric)
+    def add_log_change(self, factor, df, lookback_days, gap_days=0, k=1):        
         min_obs =  0.8
         window_days = int(lookback_days - gap_days)
         
-        return (
-            df.with_columns(
-                pl.col("log_ret")
-                .shift(gap_days) #pushed forward in time 
-                .rolling_sum(
-                    window_size=window_days,
-                    weights=None,
-                    min_samples=int(window_days*min_obs) #min_obs
-                )
-                .over("symbol")
-                .alias(factor)            
+        return df.with_columns(
+            (pl.col("log_ret")
+             .shift(gap_days) #pushed forward in time 
+             .rolling_sum( 
+                 window_size=window_days,
+                 weights=None, 
+                 min_samples=int(window_days*min_obs) #min_obs validation
+             )
+             * k
             )
+            .over("symbol")
+            .alias(factor)            
         )
                    
-    def winsor_factor(self, df, factor, p=0.01):
-        df = df.with_columns(
-            pl.col(factor).clip(
-                pl.col(factor).quantile(p).over("date"),
-                pl.col(factor).quantile(1-p).over("date")
-            ).alias(factor)
-        )
-        return df
-
-    def znorm_factor(self, df, factor):
-        df = df.with_columns([
-            ((pl.col(factor)-pl.col(factor).mean().over("date")) /
-             pl.col(factor).std().over("date"))
-            .alias(factor)
+    def winsor_factors(self, df, factors, p=0.01):
+        return df.with_columns([
+            pl.col(f).clip(
+                pl.col(f).quantile(p).over("date"),
+                pl.col(f).quantile(1-p).over("date")
+            )
+            .alias(f)
+            for f in factors
         ])
-        return df
 
-    def combine_factors(self, df, factors, name, weights=None):
-        assert isinstance(factors, list), "Should be a list"
-        n = len(factors)
-        if weights is None: weights = [1/n for i in range(n)]
+    def combine_factors(self, df, factors, composite):
+       return df.with_columns(
+               pl.mean_horizontal(factors).alias(composite) #automatic reweighting (0.5*A + 0.5*B -> 1*A + 0*B when B is null), could also do sum_horizontal / used_weights
+        ).drop(factors)
 
-        df = df.with_columns([
-            pl.sum_horizontal([pl.col(f)*w for f, w in zip(factors, weights)])
-        ]).alias(name)
-        return self.znorm_factor(df, name)
+    def znorm_factors(self, df, factors):
+        return df.with_columns([
+            (pl.col(f) - pl.col(f).mean().over("date")) / (pl.col(f).std().over("date") + + 1e-8)
+            .alias(f)
+            for f in factors
+        ])
+
+    def neutralise_factors(self, df, factors):
+        #F=BX+e, where F is raw factor, X is risk factors and B is estimated
+        #keeps mu=0, so only need to rescale sd
+        return df.with_columns( #PLACEHOLDER -> equivalent to OLS of categorical
+            (pl.col(f) - pl.col(f).mean().over(["date", "sector"]))
+            .alias(f)
+            for f in factors
+        )
+    
+    def rescale_factors(self, df, factors):
+        return df.with_columns([
+            pl.col(f) / (pl.col(f).std().over("date") + + 1e-8)
+            .alias(f)
+            for f in factors
+        ])        
+
+    def reverse_winsor(self, df, factor, p=0.05):
+        low_mask = pl.col(factor).quantile(p).over("date")
+        high_mask = pl.col(factor).quantile(1-p).over("date")
         
+        return df.with_columns(
+            pl.when(pl.col(factor).is_between(low_mask, high_mask, closed="none")) #exclusive
+            .then(pl.lit(None))
+            .otherwise(pl.col(factor))
+            .alias(factor)
+        )
+
+    def process_factor(self, df, factors, composite):
+        return (
+            df
+            #Components
+            .pipe(self.winsor_factors, factors, p=0.01) 
+            .pipe(self.znorm_factors, factors)
+            .pipe(self.combine_factors, factors, composite)
+            .pipe(self.znorm_factors, [composite])
+
+            #Composite z-normalised signal
+            .pipe(self.neutralise_factors, [composite])
+            .pipe(self.rescale_factors, [composite])
+        )
 
 symbols = ["AAPL", "META", "MSFT", "WMT", "COST", "NVDA"]
+factor_defs = {
+    "MOM": [["UMD_12_1", "UMD_6_1", "UMD_3_1"], 21, 1], #skips to ignore STR
+    "VAL": [["HML_3", "HML_5"], 21*12, -1]
+}
+#21 trading days per month
+
+#OLS equation: \hat{\beta} = (X'X)^{-1}X'y
+loader = Loader()
+processor = Processor()
 
 
-mo = 21 #trading days per month
-his = History()
-df = his.load_history(symbols)
-df = his.log_transform(df)
-df = his.add_log_change("UMD_12_1", df, 12*mo, mo)
+#Loading
+lf = loader.load_data(symbols, "Profile", fetch_func=loader.fetch_profile) 
+pf = lf.unique(subset=["symbol"], keep="last").drop("ts") #assuming read in order
+#lf = lf.sort("ts", descending=False).group_by(["symbol", "date"]).last()
 
-df = df.collect()
-print(df[["date", "log_ret", "close", "UMD_12_1"]].tail())
-
-#Factor Neutralisation
-#F=BX+e, where F is raw factor, X is risk factors and B is estimated
-#zscore composure -> neut -> rescale (x-0)/sd: x/x.std().over("date")
-#OLS equation :)
+lf = loader.load_data(symbols, "History", fetch_func=loader.fetch_history, other_args={"period": "max"})
+his = lf.unique(subset=["symbol", "date"], keep="last").drop("ts").drop(['open', 'high', 'low', 'volume', 'close', 'splits'])
 
 
-#high, low masking
+#Processing
+categories = pf.drop(["symbol"]).collect_schema().names()
+pf = pf.with_columns([ #Label Encoding
+    pl.col(cat)
+    .fill_null("Unknown")
+    .cast(pl.Categorical)
+    .to_physical() 
+    .alias(cat)
+    for cat in categories
+])
+df = his.join(pf, on="symbol", how="left").collect()
+
+lf = processor.log_transform(df) #returns lazy
+
+for factors, unit, k in factor_defs.values():
+    for factor in factors:
+        val = factor.split("_") + [0]
+        lf = processor.add_log_change(factor, lf, int(val[1])*unit, int(val[2])*unit, k=k)
+
+for composite, (factors, _, _) in factor_defs.items():
+    lf = processor.process_factor(lf, factors, composite)
+
+df = lf.collect()
+print(df.columns)
+print(df.tail())
+
+
 #unit test, smooth forward fill
 #sampling
 
